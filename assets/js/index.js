214806a91af3a14c82341519d2c8022beea30e99
GabrielMunoz <gabriel.munoz.solis@gmail.com>
2023-01-26 19:02:57 -0300

    trabajoenjs

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+const precio = 400000
+const precioSpan = document.querySelector(".precio");
+precioSpan.innerHTML = precio.toLocaleString();
+
+const cantidadInput = document.querySelector("#cantidad")
+const colorSelect = document.querySelector("#idcolor")
+
+const valorTotal = document.querySelector(".valortotal")
+const cantidadTotal = document.querySelector(".cantidadtotal")
+const colorFinal = document.querySelector(".bola div")
+
+let total = 0
+
+function calcularTotal() {
+
+
+    const cantidad = cantidadInput.value
+    const color = colorSelect.value
+
+
+    valorTotal.innerHTML = (+cantidad * precio).toLocaleString();
+    cantidadTotal.innerHTML = cantidad
+    colorFinal.style.backgroundColor = color
+}
